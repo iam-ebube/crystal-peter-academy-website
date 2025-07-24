7f5baf2d0a677fe8e277cef9b2b6a2d47e0c7417
iam-ebube <ebubeezeudu@gmail.com>
2025-07-24 14:46:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuClose = document.getElementById("menu-close");
+  const mobileNav = document.getElementById("mobile-nav");
+
+  menuToggle.addEventListener("click", function () {
+    mobileNav.style.display = "block";
+    menuToggle.style.display = "none";
+    menuClose.style.display = "block";
+  });
+
+  menuClose.addEventListener("click", function () {
+    mobileNav.style.display = "none";
+    menuToggle.style.display = "block";
+    menuClose.style.display = "none";
+  });
+});
 
