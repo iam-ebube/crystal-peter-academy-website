78de604cf1d04b728d15e8c9eb2b3049458e63f3
iam-ebube <ebubeezeudu@gmail.com>
2025-07-24 16:35:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger-icon');
+  const sidebar = document.getElementById('sidebar');
+  const closeIcon = document.getElementById('close-icon');
+
+  hamburger.addEventListener('click', () => {
+    sidebar.classList.add('active');
+  });
+
+  closeIcon.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+  });
+});
 
