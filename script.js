f0440f3b4e19dd54ea1d21119b74674ecec4f801
iam-ebube <ebubeezeudu@gmail.com>
2025-07-25 21:30:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-//side bar display
 document.addEventListener('DOMContentLoaded', function () {
   const hamburger = document.getElementById('hamburger-icon');
   const sidebar = document.getElementById('sidebar');
   const closeIcon = document.getElementById('close-icon');
+  const sidebarLinks = sidebar.querySelectorAll('a');
 
+  // Open sidebar when hamburger is clicked
   hamburger.addEventListener('click', () => {
     sidebar.classList.add('active');
   });
 
+  // Close sidebar when 'X' is clicked
   closeIcon.addEventListener('click', () => {
     sidebar.classList.remove('active');
   });
+
+  // Close sidebar when any link is clicked
+  sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      sidebar.classList.remove('active');
+    });
+  });
 });
+
 
 //carousel image slide show
 let currentSlide = 0;
